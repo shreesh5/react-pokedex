9f37e7b0ab78f072cff02175c664b7a189855d6f
Shreesh Nayak <shreeshnayak@gmail.com>
2020-08-22 16:10:08 -0700

    Added grid layout

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Grid,
+    makeStyles,
+    Card,
+    CardContent
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    pokedexContainer: {
+        paddingTop: "20px",
+        paddingLeft: "50px",
+        paddingRight: "50px"
+    }
+})
+
+const getPokemonCard = () => {
+    return (
+        <Grid item xs={12} sm={4}>
+            <Card>
+                <CardContent>
+                    Pokemon
+                </CardContent>
+            </Card>
+        </Grid>   
+    )
+}
 
 const Pokedex = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Pokedex
-        </div>
+        <React.Fragment>
+            <AppBar position="static">
+                <Toolbar />
+            </AppBar>
+            <Grid container spacing={2} className={classes.pokedexContainer}>
+                {getPokemonCard()}
+                {getPokemonCard()}
+                {getPokemonCard()}
+                {getPokemonCard()}
+            </Grid>
+        </React.Fragment>
     )
 }
 
